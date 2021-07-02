9c9476502fd20c8bee1cbeb817665bd844e82fb9
DianaAraujoG <dianac251@gmail.com>
2021-07-02 03:29:22 -0500

    Primer Mini Proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import validator from './validator.js';
+import validator from './validator.js'; /**Para que funcion ??? */
+import {mostrar, isValid} from './validator.js'; /** ./ busca desde la raiz */
 
 console.log(validator);
+
+
+var cardNumber;
+var longitud;
+/**RECUPERAMOS EL BOTON COMO OBJETO */
+const btnAccept = document.getElementById('btnAccept');
+/**Un evento para cuando le de click */
+btnAccept.addEventListener('click', (evt) => {
+/** */
+    evt.preventDefault();
+    /**Recuperamos el objeto del numero de tarjeta */
+    cardNumber = document.getElementById('cardNumber').value ;
+    console.log('boton: ' + cardNumber);
+    /*mostrar(cardNumber.value);*/ /**Prueba de llamado de funcion */
+    longitud = cardNumber.length;
+    console.log('Tamaño: '+ longitud);
+    
+    /**Validacion para que no entre vacio falta ver para que no ingrese letras */
+    if( cardNumber === '' || cardNumber.length === 0 ){
+        alert('Ingrese el numero de tarjeta')
+    }else{
+        /**Aqui se va hacer el llamado de las funciones */
+        console.log('FUNCIONES :)');
+        mostrar();
+        isValid();
+    }
+
+});
+
+/**Exportamos el numero de tarjeta y su tamaño para poder utilizarlos en el otro js */
+export {cardNumber, longitud};
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,52 @@
+import {cardNumber, longitud} from './index.js';
+
 const validator = {
   // ...
 };
 
 export default validator;
+
+
+
+
+function mostrar(){
+  console.log('Estoy en el otro js' );
+  console.log('Numero: '+cardNumber + ' Longitud: '+longitud);
+}
+function isValid(){
+  /**Declaro un arreglo para invertir los numeros de la tarjeta */
+  let array = [longitud];
+  for(let i=1; i <= longitud; i++){
+      array[longitud - i] = cardNumber.slice(i-1,i);
+      console.log(array[longitud-i]);
+  }
+  /**Simplemente para mostrar que los numeros se hallas invertido */
+  var prueba ='';
+  for(let j= 0; j<longitud; j++){
+      prueba += array[j];
+  }
+  console.log(prueba);
+  /**Se multiplica por dos los lugares pares */
+  for(let i=0 ; i < longitud ; i++){
+      if((i+1)%2 === 0){
+          array[i] = array[i]*2;
+          console.log(array[i]);
+      }
+  }
+  /**Simplemente para mostrar que los numeros se hallas invertido */
+  var prueba2 ='';
+  for(let j= 0; j<longitud; j++){
+      prueba2 += array[j];
+  }
+  console.log(prueba2);
+  /**Suma de los numeros --------------------------------------------------------------------------mayores a 9*/ 
+  /*******************************-------------------------------------------------------------aqui te quedaste*/
+  var suma;
+  for(let i=0; i< longitud; i++){
+    suma = suma + array[i];
+  }
+  console.log(suma);
+}
+/*console.log('ingreso ' + numero);*/
+
+export {mostrar, isValid};
