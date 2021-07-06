49ecec8f0856f981c1c9ab061046bc5b795d9e1d
DianaAraujoG <dianac251@gmail.com>
2021-07-06 13:10:31 -0500

    Ya en el objeto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import validator from './validator.js'; /**Para que funcion ??? */
-import {mostrar, isValid,  maskify} from './validator.js'; /** ./ busca desde la raiz */
 
-console.log(validator);
+//console.log(validator);
 
 
-var cardNumber;
-var longitud;
 /**RECUPERAMOS EL BOTON COMO OBJETO */
 const btnAccept = document.getElementById('btnAccept');
 /**Un evento para cuando le de click */
@@ -14,29 +11,23 @@ btnAccept.addEventListener('click', (evt) => {
 /** Previene las cosas o acciones raras que ya vienen en el click */
     evt.preventDefault();
     /**Recuperamos el objeto del numero de tarjeta */
-    cardNumber = document.getElementById('cardNumber').value ;
-    console.log('boton: ' + cardNumber);
-    /*mostrar(cardNumber.value);*/ /**Prueba de llamado de funcion */
-    longitud = cardNumber.length;
+    let creditCardNumber = document.getElementById('creditCardNumber').value ;
+    console.log('boton: ' + creditCardNumber);
+    /*mostrar(creditCardNumber.value);*/ /**Prueba de llamado de funcion */
+    let longitud = creditCardNumber.length;
     console.log('Tamaño: '+ longitud);
     
     /**Validacion para que no entre vacio falta ver para que no ingrese letras */
-    if( cardNumber === '' || cardNumber.length === 0 ){
+    if( creditCardNumber === '' || creditCardNumber.length === 0 ){
         alert('Ingrese el numero de tarjeta')
     }else{
         /**Aqui se va hacer el llamado de las funciones */
-        mostrar();
-        document.getElementById('mostrarValida').innerHTML = isValid();
+        document.getElementById('mostrarValida').innerHTML = validator.isValid(creditCardNumber);
         /*En la siguiente linia se muestra el codificado del numero de la tarjeta cambiando
         el contenido de un parrado con la ayuda de document by id */
-        document.getElementById('mostrarNumero').innerHTML = maskify (cardNumber);
-        
+        let codificacion = validator.maskify(creditCardNumber);
+        document.getElementById('mostrarNumero').innerHTML = codificacion ;
     }
 
 });
-
-/**Exportamos el numero de tarjeta y su tamaño para poder utilizarlos en el otro js */
-export {cardNumber, longitud};
-
-
 
--- src/validator.js
+++ src/validator.js
@@ -1,99 +1,89 @@
-import {cardNumber, longitud} from './index.js';
-
 const validator = {
-
+   //No funciona con 20
+   'maskify': function (creditCardNumber){ 
+    let codificado= ''; // aqui es donde se va a mándar el numero ya codificado
+    const longitud = creditCardNumber.length;
+    //si es de 4 o menos no hace nada y la manda igual
+    if(creditCardNumber.length <= 4){ 
+      codificado = creditCardNumber;
+    }else{
+      //
+      var array2 =[longitud];
+      for(let j=0; j<=longitud; j++){
+        array2[j]=creditCardNumber.slice(j-1,j);
+        //console.log(array2[j]);
+      }
+      let menosCuatro = longitud - 3 ;
+      for(let i=0; i< menosCuatro ; i++){
+        array2[i]='#';
+        ///console.log(array2[i]);
+      }
+      for(let i=0; i<=longitud; i++){
+        codificado += array2[i];
+        //console.log(codificado);
+      }
+    }
+    return(codificado);
+  },
+  "isValid":function(creditCardNumber){
+    /**Declaro un arreglo para invertir los numeros de la tarjeta */
+    let longitud = creditCardNumber.length;
+    let cardNumber = [longitud];
+    for(let i=1; i <= longitud; i++){
+        cardNumber[longitud - i] = creditCardNumber.slice(i-1,i);
+        console.log(cardNumber[longitud-i]);
+    }
+    /**Simplemente para mostrar que los numeros se hallas invertido */
+    var prueba ='';
+    for(let j= 0; j<longitud; j++){
+        prueba += cardNumber[j];
+    }
+    console.log(prueba);
+    /**Se multiplica por dos los lugares pares */
+    for(let i=0 ; i < longitud ; i++){
+        if((i+1)%2 === 0){
+            cardNumber[i] = cardNumber[i]*2;
+            console.log(cardNumber[i]);
+        }else{
+          continue;
+        }
+    }
+    /**Simplemente para mostrar que los numeros se hallas invertido */
+    let prueba2 ='';
+    for(let j= 0; j<longitud; j++){
+        prueba2 += cardNumber[j];
+    }
+    console.log(prueba2);
+    /**Suma de los numeros mayores a 9*/ 
+    for(let i=0 ; i<longitud; i++){
+      if((i+1)%2 === 0){
+        if(cardNumber[i] > 9){
+          cardNumber[i]= parseInt(cardNumber[i]) - 9;
+          console.log(cardNumber[i]);
+        }
+      }else{
+        continue;
+      }
+    }
+    
+    /* Suma de todos los elementos del cardNumber, pasandolos a numero*/
+    var suma=0;
+    for(let i=0; i< longitud; i++){
+      suma = suma + parseInt(cardNumber[i]);
+    }
+    /**Modulo de 10 para verificar si la tarjeta es valida o no */
+    if( suma%10 === 0){
+      return(true); /**MODIFICAR A QUE ALTERE EL CONTENIDO DE UN PARRAFO  */ /**'Tarjeta valida FELICIDADES' */
+    }else{
+      return(false); /**'NO es valida, LO SIENTO' */
+    }
+    console.log(suma);
+  },
+  
   
 };
 /*Para esto debes implementar el objeto validator, el cual ya se encuentra exportado en el boilerplate. 
 Este objeto (validator) contiene dos métodos (isValid y maskify):*/
 /**CREO QUE LO HICE MAL :( */
 
-
-
-function mostrar(){
-  console.log('Estoy en el otro js' );
-  console.log('Numero: '+cardNumber + ' Longitud: '+longitud);
-}
-
-function isValid(){
-  /**Declaro un arreglo para invertir los numeros de la tarjeta */
-  var array = [longitud];
-  for(let i=1; i <= longitud; i++){
-      array[longitud - i] = cardNumber.slice(i-1,i);
-      console.log(array[longitud-i]);
-  }
-  /**Simplemente para mostrar que los numeros se hallas invertido */
-  var prueba ='';
-  for(let j= 0; j<longitud; j++){
-      prueba += array[j];
-  }
-  console.log(prueba);
-  /**Se multiplica por dos los lugares pares */
-  for(let i=0 ; i < longitud ; i++){
-      if((i+1)%2 === 0){
-          array[i] = array[i]*2;
-          console.log(array[i]);
-      }else{
-        continue;
-      }
-  }
-  /**Simplemente para mostrar que los numeros se hallas invertido */
-  var prueba2 ='';
-  for(let j= 0; j<longitud; j++){
-      prueba2 += array[j];
-  }
-  console.log(prueba2);
-  /**Suma de los numeros mayores a 9*/ 
-  for(let i=0 ; i<longitud; i++){
-    if((i+1)%2 === 0){
-      if(array[i] > 9){
-        array[i]= parseInt(array[i]) - 9;
-        console.log(array[i]);
-      }
-    }else{
-      continue;
-    }
-  }
-  
-  /* Suma de todos los elementos del array, pasandolos a numero*/
-  var suma=0;
-  for(let i=0; i< longitud; i++){
-    suma = suma + parseInt(array[i]);
-  }
-  /**Modulo de 10 para verificar si la tarjeta es valida o no */
-  if( suma%10 === 0){
-    return('Tarjeta valida FELICIDADES'); /**MODIFICAR A QUE ALTERE EL CONTENIDO DE UN PARRAFO  */
-  }else{
-    return('NO es valida, LO SIENTO');
-  }
-  console.log(suma);
-}
-
-//No funciona con 20
-function maskify (cardNumber){ 
-  var codificado= ''; // aqui es donde se va a mándar el numero ya codificado
-  //si es de 4 o menos no hace nada y la manda igual
-  if(cardNumber.length <= 4){ 
-    codificado = cardNumber;
-  }else{
-    //
-    var array2 =[longitud];
-    for(let j=0; j<=longitud; j++){
-      array2[j]=cardNumber.slice(j-1,j);
-      //console.log(array2[j]);
-    }
-    let menosCuatro = longitud - 3 ;
-    for(let i=0; i< menosCuatro ; i++){
-      array2[i]='#';
-      ///console.log(array2[i]);
-    }
-    for(let i=0; i<=longitud; i++){
-      codificado += array2[i];
-      //console.log(codificado);
-    }
-  }
-  return(codificado);
-}
-
 export default validator;
-export {mostrar, isValid, maskify};
